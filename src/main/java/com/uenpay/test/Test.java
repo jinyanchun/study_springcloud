1c51e6aaee9ea4f0974726fcee4a675f387dfac8
jinyanchun <jinyanchun000@163.com>
2020-09-03 18:29:39 +0800

    config test

--- src/main/java/com/uenpay/test/Test.java
+++ src/main/java/com/uenpay/test/Test.java
@@ -1,5 +1,8 @@
 package com.uenpay.test;
 
+import com.uenpay.config.TestConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * @program: study_springcloud
  * @description: 测试类
@@ -7,5 +10,13 @@ package com.uenpay.test;
  * * @create: 2020-09-03 17:23
  **/
 public class Test {
-	
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TestConfig.class);
+        Test bean = context.getBean(Test.class);
+        bean.test();
+    }
+
+    public void test(){
+        System.out.println("hahaha");
+    }
 }
