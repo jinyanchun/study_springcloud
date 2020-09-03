f82044c80a66e374e3a985271734b8feefab8af2
jinyanchun <jinyanchun000@163.com>
2020-09-03 18:04:01 +0800

    idea 提交Servcie

---
+++ src/main/java/com/uenpay/service/TestService.java
@@ -0,0 +1,4 @@
+package com.uenpay.service;
+
+public interface TestService {
+}
---
+++ src/main/java/com/uenpay/service/impl/TestServiceImpl.java
@@ -0,0 +1,14 @@
+package com.uenpay.service.impl;
+
+import com.uenpay.service.TestService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @program: study_springcloud
+ * @description: 测试实现类
+ * @author: jyc
+ * * @create: 2020-09-03 18:00
+ **/
+@Service
+public class TestServiceImpl implements TestService {
+}
